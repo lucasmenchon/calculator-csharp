243c30ff9fffe6ae9d6646679db62a80c1e526ac
lucasmenchon <it.lucas@outlook.com>
2022-05-08 15:48:14 -0300

    inicio calculadora

---
+++ Calculadora/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double x, y, total;
+            string op;
+            Console.WriteLine("Soma");
+            Console.WriteLine("Digite um Número: ");
+            x = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um Operador: ");
+            op = Console.ReadLine();
+            Console.WriteLine(x + " " + op);
+            Console.WriteLine("Digite um Número: ");
+            y = double.Parse(Console.ReadLine());
+
+            switch (op)
+            {
+
+                case "+":
+                    total = x + y;
+                    break;
+
+                case "-":
+                    total = x + y;
+                    break;
+
+                case "*":
+                    total = x + y;
+                    break;
+
+                case "/":
+                    total = x + y;
+                    break;
+
+                case "%":
+                    total = x % y;
+                    break;
+
+
+                default:
+                    total = 0;
+                    break;
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
